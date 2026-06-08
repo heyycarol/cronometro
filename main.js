3ede10d5ba5a24219d2ebf81bd6286babd9b6413
heyycarol <carolina.oliveira.brito@escola.pr.gov.br>
2026-06-08 14:48:11 +0000

    aula 2

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-window.addEventListener("DOMContentLoaded", () => {
-    const botoes = document.querySelectorAll(".botao");
-    botoes.forEach((botao) => {
-        botao.addEventListener("click", () => {
-            botoes.forEach((item) => item.classList.remove("ativo"));
-            botao.classList.add("ativo");
-        });
-    });
-});
+const botoes = document.querySelectorAll(".botao");
+
+for (let i = 0; i < botoes.length; i++) {
+  botoes[i].onclick = function () {
+    for (let j = 0; j < botoes.length; j++) {
+      botoes[j].classList.remove("ativo");
+    }
+
+    botoes[i].classList.add("ativo");
+  };
+}
